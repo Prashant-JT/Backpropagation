5fe1912ae4b82a46f0561a9be571a5ece8b7dc4f
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-12-11 17:23:57 +0000

    Add files via upload

---
+++ BPNN_Model1/inputlayer.py
@@ -0,0 +1,15 @@
+import numpy
+import layer
+
+
+class InputLayer(layer.Layer):
+
+    def __init__(self, p_number_neurons=1):
+        layer.Layer.__init__(self, p_number_neurons, 1)
+
+    def init_w(self, p_random_seed=numpy.random.RandomState(None)):
+        self.w = numpy.concatenate((numpy.zeros((1, self.number_neurons)), numpy.eye(self.number_neurons)))
+        return self
+
+    def predict(self, p_X):
+        return self._net_input(p_X)
