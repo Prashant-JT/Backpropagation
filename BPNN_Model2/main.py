f296aad7408ca8814b004da03043caa812ea46a9
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-12-18 00:26:12 +0000

    Almost done

--- BPNN_Model2/main.py
+++ BPNN_Model2/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as mpl
+from matplotlib import pyplot as plt
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,7 +16,10 @@ result = {
 
 def load_dataset():
     df = pd.read_excel(r'dataset.xls')
-    df.drop(columns=['FECHA_HORA', 'TIPO_PRECIPITACION','INTENSIDAD_PRECIPITACION', 'ESTADO_CARRETERA'], inplace=True)
+    df['ACCIDENTE'] = df['ACCIDENTE'].replace(to_replace=['No', 'Yes'], value=[0, 1])
+    df.drop(columns=['FECHA_HORA', 'TIPO_PRECIPITACION', 'INTENSIDAD_PRECIPITACION', 'ESTADO_CARRETERA'],
+            inplace=True)
+    df = df.apply(pd.to_numeric, errors='coerce').dropna()
     features = df.iloc[:, 0:9].values
     label = df.iloc[:, 9].values
     return features, label
@@ -26,22 +29,39 @@ def test0():
     # Load dataset
     X, y = load_dataset()
 
-    #X = np.asarray(X).astype(int)
-    #print(X.shape)
-    #print(X[0].astype(float))
-
-    """
     # Create model
     model = Sequential()
-    model.add(Dense(24, input_dim=12, activation='relu'))
-    model.add(Dense(12, activation='relu'))
+    model.add(Dense(32, input_dim=9, activation='relu'))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(16, activation='relu'))
     model.add(Dense(1, activation="sigmoid"))
 
     # Compile and fit model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy, loss'])
-    model.fit(X, y, epochs=20, batch_size=10)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    history = model.fit(X, y, epochs=20, validation_split=0.2, shuffle=True, batch_size=30)
+
+    """
+    # plot metrics
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    """
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
 
     # Evaluate model
     _, accuracy = model.evaluate(X, y)
     print('Accuracy: %.2f' % (accuracy*100))
-    """
+
+
+
