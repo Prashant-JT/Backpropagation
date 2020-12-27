fb6c039d05f641f49cefe4f01b7f5e0e9f190d1c
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2020-12-27 23:18:27 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from BPNN_Model2.with_keras import compile_model as compile_keras, evaluate_model as evaluate_keras
-from BPNN_Model1.without_keras import compile_model as compile_no_keras
+from BPNN_Model1.without_keras import compile_model as compile_no_keras, evaluate_model as evaluate_no_keras
+from BPNN_Model1.without_keras import test_sum_dataset, predict_test_dataset
 
 
 # Run model with Keras
@@ -10,5 +11,13 @@ def run_keras():
 
 # Run model without Keras
 def run_no_keras():
-    compile_no_keras()
-    # evaluate_model()
+    model = compile_no_keras()
+    evaluate_no_keras(model)
+
+    """
+    Para probar el dataset de suma, comentar las dos 
+    llamadas anteriores, y descomentar las dos siguientes.
+    """
+
+    # model = test_sum_dataset()
+    # predict_test_dataset(model)
